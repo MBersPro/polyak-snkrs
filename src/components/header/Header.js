7a78ca79e1922f76293cf3a19b11e51ff77e3b3e
MBers <mbers.pro@gmail.com>
2022-05-08 11:26:38 +0300

    Added header container

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React from "react";
+import styles from "./Header.module.css";
 
 const Header = () => {
-    return (
-        <div>Header</div>
-    );
-}
+  return <div className={styles.container}>Header</div>;
+};
 
 export default Header;

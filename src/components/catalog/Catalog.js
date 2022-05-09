ce3c36357e7280327f5b1a2b92bc222f89e392ec
MBers <mbers.pro@gmail.com>
2022-05-09 19:37:56 +0300

    Added Footer

--- src/components/catalog/Catalog.js
+++ src/components/catalog/Catalog.js
@@ -3,8 +3,18 @@ import "./Catalog.css";
 
 const Catalog = () => {
   return (
-    <div className="container">
-      <div className="catalog_head"><h2>КАТАЛОГ</h2></div>
+    <div className="container catalog_container">
+      <div className="catalog_head">
+        <ul className="catalog_filter_container">
+          <li>
+            <h2>КАТАЛОГ</h2>
+          </li>
+          <li className="catalog_filter_li">Yeezy</li>
+          <li className="catalog_filter_li">Jordan</li>
+          <li className="catalog_filter_li">Nike</li>
+          <li className="catalog_filter_li">Kids</li>
+        </ul>
+      </div>
     </div>
   );
 };

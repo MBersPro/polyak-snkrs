979e462a7992ef82af88a87ce8b7bcc1133cca98
Muhammad Mizaev <m.u.009@mail.ru>
2022-05-09 13:11:06 +0300

    added question

--- src/components/questions/Questions.js
+++ src/components/questions/Questions.js
@@ -1,8 +1,20 @@
 import React from 'react';
-
+import "./Questions.css";
 const Questions = () => {
     return (
-        <div>Часто задаваемые вопросы</div>
+        <div className='container'>
+            <h2 className='h2'>ЧАСТО ЗАДАВАЕМЫЕ ВОПРОСЫ</h2>
+            <ul className='ul'>
+                <li className='li'>+ Как происходит оплата ?</li>
+                <li className='li'>+ Как происходит доставка ?</li>
+                <li className='li'>+ Как оформить заказ ?</li>
+                <li className='li'>+ Вся продукция оригинальная?</li>
+                <li className='li'>+ Почему цена зависит от размера ?</li>
+                <li className='li'>+ Как формируются цены на сайте ?</li>
+                <li className='li'>+ Что делать, если размер мне не подошел ?</li>
+            </ul>
+        </div>
+
     );
 }
 

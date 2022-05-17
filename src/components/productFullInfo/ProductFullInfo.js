086a17c405226f3366ba06e10b4047941011c16b
MBers <mbers.pro@gmail.com>
2022-05-17 21:33:55 +0300

    added changes

--- src/components/productFullInfo/ProductFullInfo.js
+++ src/components/productFullInfo/ProductFullInfo.js
@@ -17,37 +17,51 @@ const ProductFullInfo = ({ product, addToKorzina, changePage }) => {
   const onAddToKorzina = () => {
     addToKorzina();
   };
-  
+
   const onBack = () => {
     changePage("main");
-    document.querySelector('body').style.overflow = "scroll"
+    document.querySelector("body").style.overflow = "scroll";
   };
 
   return (
     <>
       <div className="overlay">
         <div className="container">
-        <button onClick={onBack} type="button" className="productFullInfo_back_btn">❮ Назад</button>
-            <p className="productFullInfo_name_product">
-                    <span>{product.brand}</span>
-                    <span>{product.model}</span>
-            </p>
+          <button
+            onClick={onBack}
+            type="button"
+            className="productFullInfo_back_btn"
+          >
+            ❮ Назад
+          </button>
+          <p className="productFullInfo_name_product">
+            <span>{product.brand}</span>
+            <span>{product.model}</span>
+          </p>
           <p className="productFullInfo_price_product">{product.price} ₽</p>
           <div className="productFullInfo_image_container">
-            <img
+            <ImageGallery items={productImages} showPlayButton={false} />
+            {/* <img
               className="productFullInfo_image"
               alt="sneakers"
               src={product.images[0]}
-            />
+            /> */}
           </div>
           <p className="productFullInfo_shoe_size">Выбрать размер</p>
-          <input value="3.5Y" className="productFullInfo_input_size"/>
-          <p className="productFullInfo_shoe_description">Кроссовки Air Jordan 1 Mid, дизайн которых вдохновлен оригинальной
-          <br/> моделью AJ1, дают фанатам возможность пойти по стопам Майкла
-          <br/> Джордана. Элементы отделки свежих цветов и классические материалы
-          <br/> привносят новизну в привычный образ.</p>
+          <input value="3.5Y" className="productFullInfo_input_size" />
+          <p className="productFullInfo_shoe_description">
+            Кроссовки Air Jordan 1 Mid, дизайн которых вдохновлен оригинальной
+            <br /> моделью AJ1, дают фанатам возможность пойти по стопам Майкла
+            <br /> Джордана. Элементы отделки свежих цветов и классические
+            материалы
+            <br /> привносят новизну в привычный образ.
+          </p>
           <p className="productFullInfo_color_model">Цвет модели:</p>
-          <button onClick={onAddToKorzina} type="button" className="productFullInfo_addToBasket_btn">
+          <button
+            onClick={onAddToKorzina}
+            type="button"
+            className="productFullInfo_addToBasket_btn"
+          >
             Добавить в корзину
           </button>
         </div>

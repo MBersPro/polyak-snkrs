183e414f264c0e01dfb35d7a943b92c7a47fe300
MBers <mbers.pro@gmail.com>
2022-05-26 10:44:04 +0300

    commit

--- src/components/catalog/catalogHead/CatalogHead.js
+++ src/components/catalog/catalogHead/CatalogHead.js
@@ -1,14 +1,42 @@
 import React from "react";
-import './CatalogHead.css'
+import "./CatalogHead.css";
 const CatalogHead = () => {
   return (
     <div className="catalog_head">
       <h2>КАТАЛОГ</h2>
       <ul className="catalog_filter_container">
-        <li className="catalog_filter_li">Yeezy</li>
-        <li className="catalog_filter_li">Jordan</li>
-        <li className="catalog_filter_li">Nike</li>
-        <li className="catalog_filter_li">Kids</li>
+        <li className="catalog_filter_li">
+          <select>
+            <option>Yeezy</option>
+            <option>350</option>
+            <option>450</option>
+            <option>500</option>
+            <option>700</option>
+            <option>Slide</option>
+            <option>Foam Runner</option>
+          </select>
+        </li>
+        <li className="catalog_filter_li">
+          <select>
+            <option>Jordan</option>
+            <option>1 Low</option>
+            <option>1 Mid</option>
+            <option>1 High</option>
+            <option>Jordan 4</option>
+          </select>
+        </li>
+        <li className="catalog_filter_li">
+          <select className="catalogFilter_select">
+            <option>Nike</option>
+            <option>Dunk Low</option>
+            <option>Dunk High</option>
+          </select>
+        </li>
+        <li className="catalog_filter_li">
+          <select>
+            <option>Kids</option>
+          </select>
+        </li>
       </ul>
     </div>
   );

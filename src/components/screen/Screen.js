679e9caf640c3f898a6b1af1538dcc5083447514
MBers <mbers.pro@gmail.com>
2022-06-14 16:53:47 +0300

    Added photos

--- src/components/screen/Screen.js
+++ src/components/screen/Screen.js
@@ -2,16 +2,20 @@ import React, { useEffect, useState, useMemo } from "react";
 
 import image1d from "./snkrs_image_1.jpg";
 import image2d from "./snkrs_image_2.jpg";
-import image3d from "./snkrs_image_3.jpg";
+//import image3d from "./snkrs_image_3.jpg";
 import image4d from "./snkrs_image_4.jpg";
 import image5d from "./snkrs_image_5.jpg";
+import image1t from "./snkrs_image_1t.jpg";
+import image2t from "./snkrs_image_2t.jpg";
+import image3t from "./snkrs_image_3t.jpg";
+import image4t from "./snkrs_image_4t.jpg";
 
 import styles from "./Screen.module.css";
 
 const test = {
-  mobile: [image4d],
-  tablet: [image5d],
-  desktop: [image1d, image2d, image3d],
+  mobile: [image1t, image2t, image3t, image4t],
+  tablet: [image1t, image2t, image3t, image4t],
+  desktop: [image1d, image2d, image4d, image5d],
 };
 
 let timeout = null;
@@ -25,7 +29,7 @@ const Screen = ({ children, viewPort }) => {
       timeout = setInterval(() => {
       setBgImage(imagesArray[count % imagesArray.length]);
       count += 1;
-    }, 3000);
+    }, 5000);
   };
 
   const imagesArray = useMemo(() => {
